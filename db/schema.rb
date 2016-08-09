2dceecc9f587472aa2a0aad994e55659a1fc61be
Floyd Wallace <fwallace@pacbell.net>
2016-08-09 12:02:15 -0700

    add changes schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20160803214902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sms", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "created_at"], name: "index_sms_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_sms_on_user_id"
+    t.index ["user_id", "created_at"], name: "index_sms_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_sms_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 20160803214902) do
     t.string   "salt"
     t.string   "password"
     t.string   "password_digest"
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
   end
 
 end
